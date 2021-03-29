bbd0e6ceda0003d01da45c5def380b59beae4f3b
Jasmeen241 <jasnoorkaursidhu2002@gmail.com>
2021-03-30 05:03:03 +0530

    creating toddolst completed toddolst

--- JS/app.js
+++ JS/app.js
@@ -1,1 +1,34 @@
-// your JavaScript goes here, you can delete this comment
+var toddo = {
+    "toddo": "submit web tech lab" , status: "complete",
+    "toddo": "work on sql project", status: "incomplete",
+    "toddo": "work on 1515 labs" , status:"complete",
+     "toddo":"work on maths assignment" , status:"incomplete"
+ }
+
+ var toddo = [
+    {
+        toddo:  "submit web tech lab",
+        status: 'complete'
+    },
+    {
+        toddo: "work on sql project" ,
+        status: 'started'
+    },
+    {
+        todo:  "work on 1515 labs" ,
+        status: 'started'
+    },
+    {
+        todo: "work on maths assignment"  ,
+        status: 'started'
+    }
+];
+
+var completed = [];
+
+for (var todo of toddo) {
+    if (todo.status == "complete") {
+        completedTodos.push(todo)
+    }
+}
+
